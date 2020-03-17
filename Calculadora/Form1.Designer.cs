89e28cb028409de48131901f1995caacee099665
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-03-17 21:28:53 +0100

    Calculadora casi terminada

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -44,6 +44,11 @@
             this.button13 = new System.Windows.Forms.Button();
             this.button14 = new System.Windows.Forms.Button();
             this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -182,9 +187,9 @@
             // 
             this.button10.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.button10.Font = new System.Drawing.Font("Microsoft JhengHei", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.Location = new System.Drawing.Point(126, 423);
+            this.button10.Location = new System.Drawing.Point(252, 423);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(197, 80);
+            this.button10.Size = new System.Drawing.Size(80, 80);
             this.button10.TabIndex = 12;
             this.button10.Text = "=";
             this.button10.UseVisualStyleBackColor = false;
@@ -238,11 +243,76 @@
             this.button15.UseVisualStyleBackColor = false;
             this.button15.Click += new System.EventHandler(this.button12_Click);
             // 
+            // button16
+            // 
+            this.button16.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button16.Font = new System.Drawing.Font("Microsoft JhengHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button16.Location = new System.Drawing.Point(12, 525);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(80, 80);
+            this.button16.TabIndex = 17;
+            this.button16.Text = "AC";
+            this.button16.UseVisualStyleBackColor = false;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button17.Font = new System.Drawing.Font("Microsoft JhengHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button17.Location = new System.Drawing.Point(126, 525);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(80, 80);
+            this.button17.TabIndex = 18;
+            this.button17.Text = "C";
+            this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button18.Font = new System.Drawing.Font("Microsoft JhengHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button18.Location = new System.Drawing.Point(243, 525);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(80, 80);
+            this.button18.TabIndex = 19;
+            this.button18.Text = "^";
+            this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button19
+            // 
+            this.button19.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button19.Font = new System.Drawing.Font("Microsoft JhengHei", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button19.Location = new System.Drawing.Point(364, 525);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(80, 80);
+            this.button19.TabIndex = 20;
+            this.button19.Text = "√";
+            this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button20.Font = new System.Drawing.Font("Microsoft JhengHei", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button20.Location = new System.Drawing.Point(126, 423);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(80, 80);
+            this.button20.TabIndex = 21;
+            this.button20.Text = ",";
+            this.button20.UseVisualStyleBackColor = false;
+            this.button20.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(456, 525);
+            this.ClientSize = new System.Drawing.Size(456, 641);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
             this.Controls.Add(this.button15);
             this.Controls.Add(this.button14);
             this.Controls.Add(this.button13);
@@ -283,6 +353,11 @@
         private System.Windows.Forms.Button button13;
         private System.Windows.Forms.Button button14;
         private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
     }
 }
 
--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -14,61 +14,174 @@ namespace Calculadora
     {
 
         Double operando1=0;
+        Boolean operaciones = true;//Nos indica si hay un solo operador
+        Boolean igual = false;//indica si se acaba de pulsar igual para poder introducir un nuevo número
 
-        String operacion = "";
+        String operacion = "";//Operación que realizaremos
         public Form1()
         {
             InitializeComponent();
         }
 
+        //Números y comas, para mostrar en el label1 y recogerlos
         private void button1_Click(object sender, EventArgs e)
         {
+            
             Button boton = (Button)sender;
-            if (label1.Text=="0")
+
+
+
+            if (label1.Text=="0"&&boton.Text!=",")
             {
                 label1.Text = boton.Text;
             }
             else
             {
-                label1.Text += boton.Text;
+                if (igual)//Si el igual acaba de ser pulsado al introducir otro número borramos el que había
+                {
+                    if (boton.Text == "," )
+                    {
+
+                         label1.Text = "0,";
+                         igual = false;
+                        
+                    }
+                    else
+                    {
+                        label1.Text = boton.Text;
+                        igual = false;
+                    }
+                    
+                }
+                else
+                {
+                    if (boton.Text == "," && !label1.Text.Contains(","))//Para que solo pueda haber una coma por número
+                    {
+                        label1.Text += boton.Text;
+
+                    }
+                    else if(boton.Text != ",")
+                    {
+                        label1.Text += boton.Text;
+                    }
+                    
+                }
             }
-            
-            
+
         }
 
+        //Operaciones
         private void button12_Click(object sender, EventArgs e)
         {
             Button boton = (Button)sender;
 
             operacion = boton.Text;
+            if (operaciones)
+            {//Solo se puede haber un operador por operación
+                operando1 = Convert.ToDouble(label1.Text);
 
-            operando1 = Convert.ToDouble(label1.Text);
-
-            label1.Text = "0";
+                label1.Text = "0";
+            }
+            operaciones = false;
+            igual = false;
+ 
 
         }
 
+        //Igual, realiza la operación si es posible
         private void button10_Click(object sender, EventArgs e)
         {
             if (operacion=="+")
             {
                 operando1 += Convert.ToDouble(label1.Text);
                 label1.Text = Convert.ToString(operando1);
-            }else if(operacion == "-")
+                igual = true;
+
+            }
+            else if(operacion == "-")
             {
                 operando1 -= Convert.ToDouble(label1.Text);
                 label1.Text = Convert.ToString(operando1);
+                igual = true;
+
             }
             else if (operacion == "*")
             {
                 operando1 *= Convert.ToDouble(label1.Text);
                 label1.Text = Convert.ToString(operando1);
+                igual = true;
+
             }
             else if (operacion == "/")
             {
                 operando1 /= Convert.ToDouble(label1.Text);
                 label1.Text = Convert.ToString(operando1);
+                igual = true;
+
             }
+            else if (operacion == "^")
+            {
+                operando1 = Math.Pow(operando1, Convert.ToDouble(label1.Text));
+                label1.Text = Convert.ToString(operando1);
+                igual = true;
+     
+            }
+            else if (operacion == "√")
+            {
+                operando1 = Convert.ToDouble(label1.Text.Substring(1));
+                operando1 = Math.Sqrt(operando1);
+                label1.Text = Convert.ToString(operando1);
+                igual = true;
+
+            }
+
+            operacion = "";
+            operaciones = true;
+            
         }
+
+        //Borra el último número introducido
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+            if (label1.Text.Length==1)//Si ya hay un 0 lo deja
+            {
+                label1.Text = "0";
+            }
+            else
+            {
+                label1.Text = label1.Text.Substring(0,label1.Text.Length-1);
+            }
+            
+        }
+
+        //Reinicia la calculadora
+        private void button17_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0";
+            operando1 = 0;
+
+            operaciones = true;//Nos indica si hay un solo operador
+            igual = false;
+
+            operacion = "";
+
+        }
+
+        //Raíces cuadradas
+        private void button19_Click(object sender, EventArgs e)
+        {
+
+            operando1 = Convert.ToDouble(label1.Text);
+            if (label1.Text == "0")//Si solo hay un 0 lo quitamos por estética
+            {
+                label1.Text = "";
+            }
+            label1.Text = "√"+ label1.Text;//Ponemos el "√" a principio del texto
+            operacion = "√";
+            igual = false;
+
+        }
+
     }
 }
